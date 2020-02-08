7e675b03456a8a469bdcaf8349f01ec22c948c0f
Jiajie Chen <noc@jiegec.ac.cn>
2020-02-08 22:47:51 +0800

    Can read IDCODE in Rust correctly

--- src/device.rs
+++ src/device.rs
@@ -1,5 +1,8 @@
 use super::binding::*;
+use bitvec::prelude::*;
 use std::ffi::CStr;
+
+pub type JTAGResult<T> = Result<T, String>;
 
 pub struct Device {
     context: *mut ftdi_context,
@@ -14,7 +17,7 @@ impl Device {
         }
     }
 
-    pub fn new(vid: u32, pid: u32) -> Result<Self, String> {
+    pub fn new(vid: u32, pid: u32) -> JTAGResult<Self> {
         let context = unsafe { ftdi_new() };
         let mut dev = Device { context };
 
@@ -60,13 +63,72 @@ impl Device {
         self.ftdi_write(&idle).expect("reset jtag");
     }
 
-    fn ftdi_write(&mut self, data: &[u8]) -> Result<usize, String> {
+    fn ftdi_write(&mut self, data: &[u8]) -> JTAGResult<usize> {
         let res = unsafe { ftdi_write_data(self.context, data.as_ptr(), data.len() as _) };
         if res < 0 {
             Err(self.get_error())
         } else {
             Ok(res as _)
         }
+    }
+
+    pub fn shift_dr(&mut self, output: &BitVec<Local, u8>) -> JTAGResult<BitVec<Local, u8>> {
+        let shift = [
+            (MPSSE_WRITE_TMS | MPSSE_LSB | MPSSE_BITMODE | MPSSE_WRITE_NEG) as u8,
+            0x03,
+            0x02,
+        ];
+        self.ftdi_write(&shift)?;
+
+        let mut temp = output.clone();
+        let mut input = output.clone();
+        let last_bit = temp.pop().unwrap();
+
+        let bits = temp.len();
+        let bytes = bits / 8;
+
+        if bytes > 0 {
+            let mut shift_bytes = vec![
+                (MPSSE_DO_READ | MPSSE_DO_WRITE | MPSSE_LSB | MPSSE_WRITE_NEG) as u8,
+                ((bytes - 1) & 0xff) as _,
+                ((bytes - 1) >> 8) as _,
+            ];
+            shift_bytes.extend(&temp.as_slice()[0..bytes]);
+            self.ftdi_write(&shift_bytes)?;
+        }
+
+        if bits % 8 != 0 {
+            let mut shift_bits = vec![
+                (MPSSE_DO_READ | MPSSE_DO_WRITE | MPSSE_LSB | MPSSE_WRITE_NEG | MPSSE_BITMODE) as u8,
+                ((bits % 8) -1) as _,
+            ];
+            shift_bits.push(temp.as_slice()[bytes]);
+            self.ftdi_write(&shift_bits)?;
+        }
+
+        let idle = [
+            (MPSSE_WRITE_TMS | MPSSE_LSB | MPSSE_BITMODE | MPSSE_WRITE_NEG) as u8,
+            0x02,
+            0x03 | ((last_bit as u8) << 7),
+        ];
+        self.ftdi_write(&idle)?;
+
+        let input_slice = input.as_mut_slice();
+        let mut offset = 0;
+        while offset < input_slice.len() {
+            let read_bytes = unsafe {
+                ftdi_read_data(self.context, 
+                    input_slice.as_mut_ptr().add(offset),
+                    (input_slice.len() - offset) as _,
+                )
+            };
+            if read_bytes < 0 {
+                return Err(self.get_error());
+            }
+            offset += read_bytes as usize;
+        }
+
+        Ok(input)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use bitvec::prelude::*;
 use jtag_console;
 
 fn main() {
     let dev = jtag_console::Device::new(0x0403, 0x6014);
     println!("{}", dev.is_ok());
+    let output = bitvec![Lsb0, u8; 0; 32];
+    let res = dev.unwrap().shift_dr(&output).unwrap();
+    println!("{:?}", res);
+    for bit in &*res {
+        print!("{}", *bit as usize);
+    }
 }
